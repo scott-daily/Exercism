0361d88388a57df6c6e6606f9efe3f17eb73f4a7
scott-daily <scottjuggles@gmail.com>
2021-07-12 15:06:20 -0400

    Completed gigasecond.go exercism

---
+++ go/gigasecond/gigasecond.go
@@ -0,0 +1,11 @@
+// This package allows one to add a billion seconds (a gigasecond) to a given time.
+package gigasecond
+
+import "time"
+
+func AddGigasecond(t time.Time) time.Time {
+
+	updated_time := t.Add(time.Second * 1000000000)
+
+	return updated_time
+}
